e383674178cbabc3a4777f25cd990cf741c80156
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-17 06:54:59 -0500

    adding comments

--- Product Files/product.js
+++ Product Files/product.js
@@ -1,9 +1,11 @@
+// Fetch my JSON file
 fetch("mainImages.json")
-    .then(response => response.json())
-    .then(data => {
-        const myProducts = document.getElementById("products");
-        data.forEach(item => {
-            const productHTML = `
+    .then(response => response.json()) // response is turned into a JSON file
+    .then(data => { // then run data func
+        const myProducts = document.getElementById("products"); // displays variable myProducts in product div
+        data.forEach(item => { // will loop through my JSON data
+            // what will print out on my itens
+            const productHTML = ` 
             <div class="item-container">
                 <img src="${item.image}" alt="${item.name}" />
                 <h3>${item.name}</h3>
@@ -11,9 +13,10 @@ fetch("mainImages.json")
                 <p>${item.description}</p>
             </div>
             `;
+            // generates divs in my hmtl based on my JSON data
             myProducts.innerHTML += productHTML;
         });
     })
-    .catch(error => {
+    .catch(error => { // will display error if JSON doesn't load
         console.error("Error loading JSON data:", error);
     });
